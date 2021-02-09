6d73ea9f1dd687c9b62b4e7c1f285bece0eb7f03
adebayo <kurdzsamuel@gmail.com>
2021-02-09 12:50:17 +0100

    creatin order controller and updating shop routes

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { body } = require('express-validator');
 
 const shopController = require('../controllers/shop');
+const orderController = require('../controllers/order');
 
 const router = express.Router();
 const isAuthWare = require('../middlewares/is-auth');
@@ -13,11 +15,25 @@ router.get('/products/:productId', shopController.getSingleProduct);
 
 router.get('/cart', isAuthWare, shopController.getCart);
 
-router.put('/cart', isAuthWare, shopController.addProductToCart);
+router.put(
+  '/cart',
+  isAuthWare,
+  [
+    body('productId').trim().not().isEmpty(),
+  ],
+  shopController.addProductToCart,
+);
 
 router.delete('/cart/:productId', isAuthWare, shopController.deleteCartProduct);
 
-// router.post('/create-order', shopController.postOrder);
+router.post(
+  '/create-order',
+  isAuthWare,
+  // [
+  //   body('productId').trim().not().isEmpty(),
+  // ],
+  orderController.createOrder,
+);
 
 // get only orders created by the loggedIn user
 // router.get('/orders', isAuthWare, shopController.getOrders);

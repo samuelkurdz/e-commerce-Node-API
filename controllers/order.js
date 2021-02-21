b3ad53a81c439083f7748de60417725bd77fbf12
adebayo <kurdzsamuel@gmail.com>
2021-02-21 22:03:10 +0100

    orders and checkout moved to ordercontroller from shopcontroller

--- controllers/order.js
+++ controllers/order.js
@@ -16,3 +16,13 @@
 exports.createOrder = (req, res, next) => {
   res.status(200).json({ message: 'nothin is processing' });
 };
+
+// eslint-disable-next-line no-unused-vars
+exports.getOrders = (req, res, next) => {
+  res.status(200).json({ message: 'orders are coming right up' });
+};
+
+// eslint-disable-next-line no-unused-vars
+exports.getCheckout = (req, res, next) => {
+  res.status(200).json({ message: 'you are to pay for your order man' });
+};
